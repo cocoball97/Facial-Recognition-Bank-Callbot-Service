e354c9b1857939cdf495f02f3bb624109445cb97
Gimun Lee <wlrn0514@gmail.com>
2024-05-10 12:10:05 +0900

    update

--- nlp/config.py
+++ nlp/config.py
@@ -4,8 +4,8 @@ class Config(object):
     TESTING = False
 
 class DevelopmentConfig(Config):
-    organization_KEY = "org-evaR5kuPlH2xUy0vHjaZG2MY"
-    OPENAI_KEY = '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'
+    organization_KEY = "==="
+    OPENAI_KEY = '==='
     
 
 config = {
